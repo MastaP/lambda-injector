f774be7225ee4cca7b781d43d1778850713a5e87
Pavel Grigorenko <pavel.grigorenko@zeroturnaround.com>
2016-07-19 00:24:38 +0300

    Banner

--- src/main/java/org/pgrigorenko/lambdainjector/Premain.java
+++ src/main/java/org/pgrigorenko/lambdainjector/Premain.java
@@ -8,6 +8,11 @@ import java.lang.instrument.Instrumentation;
 public class Premain {
 
   public static void premain(String agentArgs, Instrumentation instr) {
+    System.out.print(
+          "******************************\n"
+        + "** Lambda Injector Starting **\n"
+        + "******************************\n"
+    );
     instr.addTransformer(new Patcher());
   }
 }
